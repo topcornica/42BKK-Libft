4e6067d805fcf1de6d3b72f824d570d60151ae22
Nakarin Rujipunya <nrujipun@z2t10c3.42bangkok.com>
2022-03-12 15:49:40 +0700

    E

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: nrujipun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/23 20:42:51 by nrujipun          #+#    #+#             */
-/*   Updated: 2022/03/01 09:48:14 by nrujipun         ###   ########.fr       */
+/*   Updated: 2022/03/12 15:49:11 by nrujipun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,22 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 
 	i = 0;
 	j = 0;
+	k = 0;
 	if (!*needle)
 		return ((char *)haystack);
-	while (*(haystack + j) != '\0' && j < len)	
+	while (*(haystack + j) != '\0' && j < len)
 	{
 		k = j;
-		while (*(needle + i) == *(haystack + j))
+		while (*(needle + i) == *(haystack + j) && needle[i] != 0
+			&& j < len)
 		{
-			while ((*(needle + i) == *(haystack + j))
-					&& ((needle[i++] != 0 
-						&& haystack[j++] != 0)
-						&& j < len))
-				continue;
-			if (needle[i] == 0)
-				return ((char *)&haystack[j]);
+			i++;
+			j++;
 		}
+		if (needle[i] == 0)
+			return ((char *)&haystack[k]);
 		i = 0;
-		j = k + 1;
+		j = k;
 		j++;
 	}
 	return (0);
