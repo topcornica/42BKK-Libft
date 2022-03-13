dfc9f65118da685f23e7a1c2eecede2bd1373465
Nakarin Rujipunya <nrujipun@z3t11c3.42bangkok.com. 3.11.13.10.in-addr.arpa name = z3t11c4.42bangkok.com>
2022-03-13 16:12:13 +0700

    ft

--- ft_substr.c
+++ ft_substr.c
@@ -6,7 +6,7 @@
 /*   By: nrujipun <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/24 15:56:49 by nrujipun          #+#    #+#             */
-/*   Updated: 2022/02/24 16:13:16 by nrujipun         ###   ########.fr       */
+/*   Updated: 2022/03/13 16:11:45 by nrujipun         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,21 @@ char	*ft_substr(const char *s, unsigned int start, size_t len)
 	i = 0;
 	if (len < 0)
 		return (0);
-	if (ft_strlen(s) < start)
-		return (0);
-	substr = (char *)malloc(len * sizeof(char));
-	while (*(s + start + i) != '\0' && i < len)
+	else if (ft_strlen(s) < start)
 	{
-		*(substr + i) = *(s + start + i);
-		i++;
+		substr = (char *)malloc(sizeof(char));
+		substr[0] = '\0';
+		return (substr);
 	}
-	*(substr + i) = '\0';
+	else
+	{
+		substr = (char *)malloc(len * sizeof(char));
+		while (*(s + start + i) != '\0' && i < len)
+		{
+			*(substr + i) = *(s + start + i);
+			i++;
+		}
+		*(substr + i) = '\0';
+	}
 	return (substr);
 }
